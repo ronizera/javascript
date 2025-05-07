2f835617881d4a2e1e7eca6d23e669f0937a647e
ronizera <roniltonbreno@gmail.com>
2025-05-06 22:48:32 -0300

    condicao simples

---
+++ aula11/ex008.js
@@ -0,0 +1,12 @@
+var vel = 60.5
+console.log(`a velocidade do seu carro é ${vel} km/h`)
+
+if(vel >= 60){
+    console.log(`Voce ultrapassou o limite de velocidade. MULTADO`)
+}
+
+else{
+    console.log(`Voce esta na velocidade perimitida`)
+}
+
+console.log(`Dirija sempre usando cinto de seguranca`)
